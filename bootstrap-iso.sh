50221152753ed22e4ddd1d890637717dfe0547ed
Christopher A. Williamson <home@chrisaw.com>
2017-08-06 22:26:11 +0100

    initrd image will now only be copied when an image to replace is in the same folder as kernel

--- bootstrap-iso.sh
+++ bootstrap-iso.sh
@@ -57,7 +57,9 @@ rm -rf \
 # copy kernels in to place
 while read -r KERNEL_PATH; do
   INITRD_PATH=$(find $(dirname ${KERNEL_PATH}) -maxdepth 1 -type f -regex '.*\(img\|lz\|gz\)$' -print -quit)
-  cp -L ${TMPDIR}/squashfs/boot/initrd.img-*bootstrap ${INITRD_PATH}
+  if [ ! -z ${INITRD_PATH} ]; then
+    cp -L ${TMPDIR}/squashfs/boot/initrd.img-*bootstrap ${INITRD_PATH}
+  fi
   cp -L ${TMPDIR}/squashfs/boot/vmlinuz-*-bootstrap ${KERNEL_PATH}
 done <<< "${KERNEL_PATHS}"
 
