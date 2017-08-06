c3fe26e322c2dcbbd798f5b82e5c02cc95255130
Christopher A. Williamson <home@chrisaw.com>
2017-08-06 08:13:18 +0100

    added fix for apt-key command

--- bootstrap-system.sh
+++ bootstrap-system.sh
@@ -41,7 +41,7 @@ elif [ -f /usr/bin/apt-get ]; then
   apt-get update
   apt-get -y install dirmngr
   echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' > /etc/apt/sources.list.d/ansible.list
-  apt-key --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
   apt-get update
   apt-get -y install ansible git
 elif [ -f /usr/sbin/emerge ]; then
