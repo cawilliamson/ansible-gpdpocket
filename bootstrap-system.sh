3fe86eeebc671dbba6de0d6cbe2365f2d7ae7b47
Christopher A. Williamson <home@chrisaw.com>
2017-08-06 08:01:38 +0100

    added dir creation for apt sources.list.d just in case!

--- bootstrap-system.sh
+++ bootstrap-system.sh
@@ -37,6 +37,7 @@ elif [ -f /usr/bin/apt-get ]; then
   done
   DEBIAN_FRONTEND=noninteractive
   sed -i 's,main restricted,main restricted universe multiverse,g' /etc/apt/sources.list
+  mkdir -p /etc/apt/sources.list.d
   echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' > /etc/apt/sources.list.d/ansible.list
   apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
   apt-get update
