9428f5c736e5890a211b360330595f4d3075e2d5
Christopher A. Williamson <home@chrisaw.com>
2017-08-10 20:16:50 +0100

    reverted some changes to bootstrap-iso so that it can be merged to master without breakages

--- bootstrap-iso.sh
+++ bootstrap-iso.sh
@@ -25,12 +25,12 @@ elif [ -f /usr/bin/apt-get ]; then
   echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' > /etc/apt/sources.list.d/ansible.list
   apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
   apt-get update
-  apt-get -y install ansible # git squashfs-tools xorriso
+  apt-get -y install ansible git squashfs-tools xorriso
 elif [ -f /usr/bin/yum ]; then
-  yum install -y ansible # git squashfs-tools xorriso
+  yum install -y ansible git squashfs-tools xorriso
 elif [ -f /usr/sbin/emerge ]; then
   emerge --sync
-  USE="blksha1 curl lz4 lzma lzo webdav xz" emerge app-admin/ansible # dev-vcs/git dev-libs/libisoburn sys-fs/squashfs-tools
+  USE="blksha1 curl lz4 lzma lzo webdav xz" emerge app-admin/ansible dev-vcs/git dev-libs/libisoburn sys-fs/squashfs-tools
 fi
 
 # extract iso
